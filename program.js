6fbe9dce49668f611970a04dd1a94d5e1881963d
hector david segura <hsdsegura6@gmail.com>
2019-05-19 17:11:32 -0500

    se agregaron los botones de color (2) y se le puso bordes al canvas sin una función

--- program.js
+++ program.js
@@ -1,37 +1,36 @@
 var square = document.getElementById("space");
-var paper = square.getContext("2d");
-var plus = document.getElementById("plus");
-var less = document.getElementById("minus"); 
 square.addEventListener("mousemove", dibujoMouse);
 square.addEventListener("mousedown", dibujoMouse);
-plus.addEventListener("click", more);
-less.addEventListener("click", substract);
+var paper = square.getContext("2d");
+var line = document.getElementById("size");
 
-dibujarLinea("black", 0, 0, square.width, 0, paper);
-dibujarLinea("black", square.width, 0, square.width, square.height, paper);
-dibujarLinea("black", square.width, square.height, 0, square.height, paper);
-dibujarLinea("black", 0, square.height, 0, 0, paper);
-
+var ini = 0
 var w=1;
+var x;
+var y;
+var colorLinea;
 
-function more ()
-{
-  w = w + 1;
-  console.log(w);
+function cambio() {
+  colorLinea= "black";
 }
-
-function substract ()
-{
-  if (w > 1)
-  {
-    w = w - 1;
-    console.log(w);
+function cambiarColor() {
+  colorLinea="aquamarine"
+}
+function wideLine(event) {
+  if (click=true) {
+    w=parseInt(line.value);
+    console.log(w)
   }
-
 }
-
-function dibujarLinea (color, xi, yi, xf, yf, lienzo)
-{
+function more (){
+  w = w + 1;
+}
+function substract (){
+  if (w > 1){
+    w = w - 1;
+  }
+}
+function dibujarLinea (color, xi, yi, xf, yf, lienzo){
   lienzo.beginPath();
   lienzo.strokeStyle = color;
   lienzo.lineWidth = w;
@@ -40,18 +39,9 @@ function dibujarLinea (color, xi, yi, xf, yf, lienzo)
   lienzo.stroke();
   lienzo.closePath();
 }
-
-var ini = 0
-var x;
-var y;
-
-function dibujoMouse (evento)
-{
-  var colorLinea = "blue"; 
-  if (evento.buttons == 1)
-  {
-    switch (ini)
-    {   
+function dibujoMouse (evento){
+  if (evento.buttons == 1){
+    switch (ini){   
       case 0:
       dibujarLinea(colorLinea, evento.layerX, evento.layerY, evento.layerX - w / 2, evento.layerY, paper);
       ini = ini + 1;
@@ -65,8 +55,7 @@ function dibujoMouse (evento)
       break; 
     }
   }
-  else
-  {
+  else{
     ini = 0;
   }
 }
